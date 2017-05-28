61e0190bcb0d664a951a6fb532778ca674760d0d
Mark Richards <Mark Richards>
2017-05-28 22:26:28 +0100

    Optimising solution by finalising pen and paper method.

--- main.py
+++ main.py
@@ -1,88 +1,10 @@
 import time
 from sudoku_solver import SudokuSolver
 
-
-
-
-
-
-
-
-
-
-# def solve_by_pen_and_paper(original_grid):
-# 	# Iterate through 1-9 asserting whether they can definitely be placed into a position
-# 	# A number can be used with complete certainty if there is only one remaining space on the axis or block
-# 	# Otherwise a number can only be used with certainity if it cannot be used in any other cell on the x/y axis and the block region
-# 	current_grid = list(original_grid)
-# 	for test_val in range (1,10):
-# 		#get set of all possible coords for the given test value
-# 		#valid_cells = get_all_possible_cells(current_grid, test_val)
-
-# 		#for any cell which is still valid, check if the test value can be used without any doubts
-# 		for y in range(0,len(current_grid)):
-# 			col_vals = current_grid[y]
-# 			for x in range(0,len(col_vals)):
-# 				if current_grid[y][x]!='_' or not is_cell_valid(x,y, test_val):
-# 					continue
-
-# 				region_number = get_region_number(x,y)
-# 				#if the row, column or region only has one remaining number then we can deduce that this number is the only valid number left
-# 				if len(all_rows[y])==8 or len(all_columns[x])==8 or len(all_regions[region_number])==8:
-# 					current_grid[y][x] = test_val
-# 					add_to
-# 					add_to_sets(x,y,test_val)
-# 					continue
-
-# 				valid = True
-# 				row = current_grid[y]
-# 				column = row[x]
-# 				for i in row:
-# 					if is_cell_valid(x,y,test_val):
-# 						valid = False
-# 						break
-
-# 				for i in column:
-# 					if is_cell_valid(x,y,test_val):
-# 						valid = False
-# 						break
-# 				for i in region:
-# 					if is_cell_valid(x,y,test_val):
-# 						valid = False
-# 						break
-# 				if valid==True:
-# 					current_grid[y][x] = test_val
-# 					add_to_sets(x,y,test_val)
-
-# 	return current_grid
 					
-
-# # Initilise quick lookup collections
-# no_remaining_cells = 0
-# for j in range(0,len(grid)):
-# 	values = grid[j]
-# 	for i in range(0, len(values)):
-# 		curr_value = values[i]
-# 		if curr_value =='_':
-# 			no_remaining_cells+=1
-# 			continue
-# 		curr_value = int(curr_value)
-# 		if not i in all_columns:
-# 			all_columns[i] = set()
-
-# 		all_columns[i].add(curr_value)
-# 		region_number = get_region_number(i, j)
-		
-# 		if region_number not in all_regions:
-# 			all_regions[region_number] = set()
-
-# 		all_regions[region_number].add(curr_value)
-# 		all_rows[j].add(curr_value)
-
 #deterministic pen and paper method followed by brute force approach
 init_start = time.time()
 #first_grid = solve_by_pen_and_paper(grid)
-#display_sudoku_grid(first_grid)
 #isplay_sudoku_grid(grid)
 #solve_by_pen_and_paper(grid)
 solver = SudokuSolver('sudoku.txt')
@@ -90,7 +12,8 @@ init_end = time.time()
 init_time = init_start - init_end
 solver.display_sudoku_grid()
 solve_start = time.time()
-solver.backtracking_search()
+solver.solve()
+#solver.brute_force(0,0)
 solve_end = time.time()
 solver.display_sudoku_grid()
 print ("Total time to solve: ",(init_end-init_end)+(solve_end-solve_start))
--- sudoku_solver.py
+++ sudoku_solver.py
@@ -16,20 +16,32 @@ class SudokuSolver:
 
 			rows = sudoku_file.readlines()
 			self._grid = []
-
+			self._original_grid = []
 			# Sanitize and display sudoku grid 
 			for j in range(0, len(rows)):
 				curr_row = rows[j]
 				sanitised_row = curr_row.replace('\n','')
 				values = sanitised_row.split(' ')
-				for i in range(len(values)):
+				self._grid.append([])
+				self._original_grid.append([])
+				for i in range(0, len(values)):
 					curr_val = values[i]
 					if curr_val!='_':
-						self.add_to_sets(i, j, int(curr_val))
-				self._grid.append(values)
+						self.add_to_sets(i,j, curr_val)
+					self._grid[j].append(curr_val)
+					self._original_grid[j].append(curr_val)
 		except IOError:
-			raise 
-		
+			raise
+
+	def set_cell_value(self, x, y, value):
+		if value=='_':
+			curr_val = self._grid[y][x]
+			self.remove_from_sets(x,y, curr_val)
+		else:
+			self.add_to_sets(x, y, value)
+		self._grid[y][x] = value
+
+	# funnction to add value to hash tables representing distinct regions, columns and rows
 	def add_to_sets(self, x, y, value):
 		region_number = self.get_region_number(x,y)
 		if region_number not in self._all_regions:
@@ -45,6 +57,7 @@ class SudokuSolver:
 			self._all_rows[y] = set()
 		self._all_rows[y].add(value)
 
+	# function remotes value from region, column and row hash tables
 	def remove_from_sets(self, x, y, value):
 		region_number = self.get_region_number(x, y)
 		self._all_columns[x].remove(value)
@@ -60,46 +73,27 @@ class SudokuSolver:
 			region_number = region_x+((region_y-1)*3)
 		return int(region_number)
 
-	def get_empty_cells_on_axis_and_region(self, x, y, exclude_input_cell):
-		empty_cells = set()
-		region_number = self.get_region_number(x,y)
-		row = self._grid[y]
-		for i in range(0,len(row)):
-			if row[i]=='_':
-				empty_cells.add((i,y))
-
-		for j in range(0, len(self._grid)):
-			row = self._grid[j]
-			if row[x]=='_':
-				empty_cells.add((i,y))
-		region_start_x = (region_number*3)-3
-		region_start_y = region_number
-		# get cells from region here
-		if exclude_input_cell==True:
-			empty_cells.remove((x,y))
-
-		return empty_cells
-
 	def display_sudoku_grid(self):
 		max_width = 9
 		header = "{0} ".format("# "*(max_width+2))
 		print (header)
 
 		for row in self._grid:
-			#print (" ".join(row))
-			current_line ="{0}{1}{2}".format("# "," ".join(row)," #")
+			current_line ="{0}{1}{2}".format("# "," ".join(str(x) for x in row)," #")
 			print (current_line)
 
 		footer = "{0} ".format("# "*(max_width+2))
 		print (footer)
 
+	# cell entry is valid if no other entry exists in the same row, column and 3x3 region
+	# calling set_cell_value will add value to these collections
 	def is_cell_valid(self, x, y, value):
-		# cell entry is valid if no other entry exists in the same row, column and 3x3 region
 		region_number = self.get_region_number(x,y)
 		if value in self._all_rows[y] or value in self._all_columns[x] or value in self._all_regions[region_number]:
 			return False
 		return True
 
+	# get next position in the grid as if it was being read from left to right, top to bottom
 	def get_next_pos(self, x, y):
 		x=(x+1)%9
 		if x==0:
@@ -107,92 +101,57 @@ class SudokuSolver:
 		return (x,y)
 
 	# function checks if any cascading number of changes from a given position can result in a valid solution
-	def is_valid_solution(self, original_grid, x, y):
+	def brute_force(self, x, y):
 		# terminate where no other cells are remaining
 		if x==0 and y==9:
 			return True
 		next_pos = self.get_next_pos(x, y)
 		# if a value at this position already exists then continue to check the next position
-		original_val = original_grid[y][x]
+		original_val = self._original_grid[y][x]
 		if original_val!='_':
-			return self.is_valid_solution(original_grid, next_pos[0], next_pos[1])
-
+			return self.brute_force(next_pos[0], next_pos[1])
 		# otherwise if no value exists at this position, test values 1-9
-		# if no value is possible, return False no possible solution 
+		# if no value is possible, return False no possible solution and backtrack
 		for curr_val in range(1, 10):
-			# if a value can be entered here, test to see if the solution persists further 
-			if self.is_cell_valid(x, y, curr_val):
-				self.add_to_sets(x, y, curr_val)
-				self._grid[y][x] = str(curr_val)
-				if self.is_valid_solution(original_grid, next_pos[0], next_pos[1]):
+			# if a value can be entered here, test to see if the solution persists further
+			if self.is_cell_valid(x, y, str(curr_val)):
+				self.set_cell_value(x, y, str(curr_val))
+				if self.brute_force(next_pos[0], next_pos[1]):
 					return True
-				# otherwise, back-track by removingfrom sets and resetting figure in the cell
-				self.remove_from_sets(x,y, curr_val)
-				self._grid[y][x]='_'
-
+				self.set_cell_value(x, y,'_')
+				
 		return False
 
-	def backtracking_search(self):
-		# 1) Search through all empty tiles, pick 1 in each check if it conforms to sudoku rules/constraints
-		# 2) If any tile doesn't comply add 1, otherwise continue with addition and repeat the process
-		# 3) If it is clear no number from 1-9 can be used in that tile then this is no longer a valid grid and requires
-		original_grid = list(self._grid)
-		#self.solve_by_pen_and_paper()
-		self.is_valid_solution(original_grid, 0, 0)
+	def solve(self):
+		# solve by pen and papr, then follow up with backtracking brute force approach
+		self.pen_and_paper_candidate_checks()
+		self.original_grid = list(self._grid)
+		self.brute_force(0,0)
 
-	# mark-up each cell with numbers 1-9
-	# iterate through numbers 1-9 while the last pass successfully added a number or this is the first pass
-	# if a cell is not valid for a number remove that number from the mark-up set and do this for all cells
-	# mark-up cells again when moving onto the next number
+	def pen_and_paper_candidate_checks(self):
+		# use a pen and paper method to find if any numbers are garunteed to be in a cell
+		change_found = True
+		while change_found:
+			change_found = False
 
-	def solve_by_pen_and_paper(self):
-		current_grid = list(self._grid)
-		solution_found = False # flag to denote if a valid number was found
-		# repeat entering numbers until no more definite numbers can be found
-		while solution_found:
-			solution_found = False
-			for test_val in range (1,10):
-				#get set of all possible coords for the given test value
-				#valid_cells = get_all_possible_cells(current_grid, test_val)
+			for y in range(0, len(self._grid)):
+				row_vals = self._grid[y]
+				for x in range(0, len(row_vals)):
+					if self._grid[y][x]!='_':
+						continue
 
-				#for any cell which is still valid, check if the test value can be used without any doubts
-				for y in range(0,len(current_grid)):
-					col_vals = current_grid[y]
-					for x in range(0,len(col_vals)):
-						if current_grid[y][x]!='_' or not is_cell_valid(x, y, test_val):
-							continue
+					# set possible values to all numbers from 1-9 
+					possible_vals = set(str(n) for n in range(1,10))
+					# remove values given the local region, row and column
+					region_number = self.get_region_number(x,y)
+					regional_entries = self._all_regions[region_number]
+					row_entries = self._all_rows[y]
+					col_entries = self._all_columns[x]
+					possible_vals = possible_vals - regional_entries - row_entries - col_entries
 
-						region_number = get_region_number(x,y)
-						#if the row, column or region only has one remaining number then we can deduce that this number is the only valid number left
-						if len(all_rows[y])==8 or len(all_columns[x])==8 or len(all_regions[region_number])==8:
-							add_to_sets(x,y,test_val)
-							current_grid[y][x] = test_val
-							solution_found = True
-							continue
-
-						# assume this number is 100% valid until other valid cells are fund in the same row, column or 3x3 region
-						row = current_grid[y]
-						column = row[x]
-						region = all_regions[region_number]
-						valid = True
-						for i in row:
-							if is_cell_valid(x,y,test_val):
-								valid = False
-								break
-
-						for i in column:
-							if is_cell_valid(x,y,test_val):
-								valid = False
-								break
-
-						for i in region:
-							if is_cell_valid(x,y,test_val):
-								valid = False
-								break
-
-						if valid!=True:
-							add_to_sets(x,y,test_val)
-							current_grid[y][x] = test_val
-							solution_found = True
-
-		return current_grid
+					# if only one possible value, update the value of this cell
+					if len(possible_vals)==1:
+						new_val = str(possible_vals.pop())
+						self.set_cell_value(x,y, new_val)
+						change_found = True
+						continue
